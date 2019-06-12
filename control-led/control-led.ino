9085bb47fd86637fe70fa26e9ef8e28c912b8cde
expoli <zzutcy.top>
2019-06-12 11:12:06 +0800

    完成功能设计

--- control-led/control-led.ino
+++ control-led/control-led.ino
@@ -26,8 +26,8 @@
 // 核心->阀设置工作状态	“xiaojiazhikong,2,[$DEVICE_NAME], [$DEVICE_NAME2],7,1,[$DEVICE_STATE],1,end”
 
 #ifndef STASSID
-#define STASSID "Xiaojia" //SSID
-#define STAPSK "mmhjd4321"       //SSID-PASSWORD
+#define STASSID "Xiaojia"  //SSID
+#define STAPSK "mmhjd4321" //SSID-PASSWORD
 #define CORE_IP "255.255.255.255"
 #define MAX_DELAY_TIME 5000
 #endif
@@ -35,6 +35,7 @@
 unsigned int localPort = 8886; // local port to listen on
 unsigned int remote_test_Port = 8888;
 unsigned int wait_time = 0;
+unsigned int connect_sign = 0;
 // buffers for receiving and sending data
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
 char ReplyBuffer[] = "acknowledged\r\n";   // a string to send back
@@ -43,25 +44,15 @@ WiFiUDP Udp;
 
 void setup()
 {
-    Serial.begin(9600);          // 串口波特率设置
-    WiFi.mode(WIFI_STA);         // WiFi工作模式
-    WiFi.begin(STASSID, STAPSK); // 连接网络
-    while (WiFi.status() != WL_CONNECTED&&wait_time<=10)
-    {
-        Serial.print('.');
-        delay(500);
-        wait_time++;
-    }
-    wait_time = 0;
-    Serial.print("Connected! IP address: ");             // 连接完成提示
-    Serial.println(WiFi.localIP());                      // 获取并打印IP
-    Serial.printf("UDP server on port %d\n", localPort); // 本地udp 监听端口
-    Udp.begin(localPort);                                // 开始udp 通信
-    pinMode(D6, OUTPUT);                                 // set onboard LED as output
+    Serial.begin(9600); // 串口波特率设置
+
+    connect_net();
+
+    pinMode(D6, OUTPUT); // set onboard LED as output
 }
 
 // 阀->核心请求连接	“xiaojiazhikong,3,[$DEVICE_NAME],CORE,0,0,1,end”
-long workstatus = 2500;
+long workstatus = 0;
 
 void loop()
 {
@@ -76,10 +67,15 @@ void loop()
     String StrStatusBuf;
 
     String ip_remote;
-    int D6_status = 0;  // D6 状态值
-    int received_sign = 0;  // 接收标志
+    int D6_status = 0;     // D6 状态值
+    int received_sign = 0; // 接收标志
 
     int packetSize = Udp.parsePacket();
+
+    if (connect_sign == 0)
+    {
+        connect_net();
+    }
 
     if (packetSize)
     {
@@ -100,18 +96,18 @@ void loop()
         for (int i = 0; i < 4; i++)
         {
             ip_remote += remote[i];
-            if (i<3)
+            if (i < 3)
             {
                 ip_remote += ".";
             }
         }
-        // 
-        ip_remote.toCharArray(charBuf,100);
+        //
+        ip_remote.toCharArray(charBuf, 100);
         Serial.print(",端口：");
-        Serial.println(Udp.remotePort());               // 输出远程端端口
+        Serial.println(Udp.remotePort()); // 输出远程端端口
 
         int remote_port = Udp.remotePort();
-        
+
         Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE); // 接收数据
         Serial.print("内容:");
         Serial.println(packetBuffer); // 输出内容
@@ -122,7 +118,6 @@ void loop()
             Serial.println("接收到控制信号");
             received_sign = 1;
         }
-
     }
 
     if (received_sign)
@@ -149,14 +144,14 @@ void loop()
             StrStatusBuf += "xiaojiazhikong,3,IOT,CORE,6,1,";
             StrStatusBuf += workstatus;
             StrStatusBuf += ",1,end";
-            StrStatusBuf.toCharArray(charBuf,100);
+            StrStatusBuf.toCharArray(charBuf, 100);
             Udp.beginPacket(CORE_IP, remote_test_Port);
             Udp.write(charBuf);
             Udp.endPacket();
 
             Serial.print("-> CORE 工作状态已返回");
-            Serial.println(workstatus>>1);
-            
+            Serial.println(workstatus >> 1);
+
             Serial.print("数据包长度：");
             Serial.println(StrPacketBuffer.length());
             StrPacketBuffer.~String();
@@ -164,12 +159,12 @@ void loop()
         // 核心->阀设置工作状态	“xiaojiazhikong,2,[$DEVICE_NAME], [$DEVICE_NAME2],7,1,[$DEVICE_STATE],1,end”
         else if (StrPacketBuffer.startsWith("xiaojiazhikong,2,CORE,IOT,7,1,"))
         {
-            StrStatusBuf = StrPacketBuffer.substring(30,34);
+            StrStatusBuf = StrPacketBuffer.substring(30, 34);
             workstatus = StrStatusBuf.toInt();
             StrStatusBuf = "xiaojiazhikong,3,IOT,CORE,6,1,";
             StrStatusBuf += workstatus;
             StrStatusBuf += ",1,end";
-            StrStatusBuf.toCharArray(charBuf,100);
+            StrStatusBuf.toCharArray(charBuf, 100);
             Udp.beginPacket(CORE_IP, remote_test_Port);
             Udp.write(charBuf);
             Udp.endPacket();
@@ -182,17 +177,51 @@ void loop()
             StrPacketBuffer.~String();
         }
     }
-    digitalWrite(D6, 1);        // D1 置1
-    D6_status = digitalRead(D6);    // 读取D6 引脚状态
-    // Serial.println("EX:D6 状态已设置为为:");
-    // Serial.println(D6_status);      // 打印现在的状态
-    delay(MAX_DELAY_TIME-workstatus);           // 继电器通电两秒
 
-    digitalWrite(D6, 0);            // D6 端口置0
-    D6_status = digitalRead(D6);    // 读取D6 引脚状态
-    // Serial.println("EX:D6 状态已设置为为:");
-    // Serial.println(D6_status);      // 打印现在的状态
-    delay(workstatus);
+
+    if (connect_sign==0)
+    {
+        digitalWrite(D6, 1);         // D1 置1
+        D6_status = digitalRead(D6); // 读取D6 引脚状态
+        // Serial.println("EX:D6 状态已设置为为:");
+        // Serial.println(D6_status);      // 打印现在的状态
+        delay(MAX_DELAY_TIME - workstatus); // 继电器通电两秒
+    }
+    else
+    {
+        digitalWrite(D6, 1);         // D1 置1
+        D6_status = digitalRead(D6); // 读取D6 引脚状态
+        // Serial.println("EX:D6 状态已设置为为:");
+        // Serial.println(D6_status);      // 打印现在的状态
+        delay(MAX_DELAY_TIME - workstatus); // 继电器通电两秒
+        digitalWrite(D6, 0);         // D6 端口置0
+        D6_status = digitalRead(D6); // 读取D6 引脚状态
+        // Serial.println("EX:D6 状态已设置为为:");
+        // Serial.println(D6_status);      // 打印现在的状态
+        delay(workstatus);
+    }
+}
+
+void connect_net(void)
+{
+    WiFi.mode(WIFI_STA);         // WiFi工作模式
+    WiFi.begin(STASSID, STAPSK); // 连接网络
+    while (WiFi.status() != WL_CONNECTED && wait_time <= 10)
+    {
+        Serial.print('.');
+        delay(500);
+        wait_time++;
+    }
+    if (wait_time <= 10)
+    {
+        Serial.print("Connected! IP address: ");             // 连接完成提示
+        Serial.println(WiFi.localIP());                      // 获取并打印IP
+        Serial.printf("UDP server on port %d\n", localPort); // 本地udp 监听端口
+        Udp.begin(localPort);                                // 开始udp 通信
+        connect_sign = 1;
+        workstatus = MAX_DELAY_TIME / 2;
+    }
+    wait_time = 0;
 }
 
 /*
